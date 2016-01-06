b5b6c467f54c23e82e898779603cf32b535e0e91
apraturu <apraturu@calpoly.edu>
2016-01-05 18:48:51 -0800

    adding stuff

---
+++ lab1/beFuddled/Action.java
@@ -0,0 +1,10 @@
+public class Action {
+   //components
+   private String actionType;
+   private int actionNumber;
+   private Location location;
+   private int pointsAdded;
+   private int points;
+   private String gameStatus;
+   private Move move;
+}
---
+++ lab1/beFuddled/Record.java
@@ -0,0 +1,11 @@
+/**
+ * Class Record holds data that is contained within a record for the game
+ * "Befuddled". A record has game status information and information per-
+ * taining to a particular move.
+ */
+public class Record {
+   //components
+   private String user;
+   private int game;
+   private Action action;
+}
