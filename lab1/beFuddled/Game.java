baed5d8e91a4a3126aeb3d09910c1787072756ed
apraturu <apraturu@calpoly.edu>
2016-01-08 14:30:14 -0800

    added method to game.java

--- lab1/beFuddled/Game.java
+++ lab1/beFuddled/Game.java
@@ -83,4 +83,8 @@ public class Game {
    public int getMaxMoves() {
       return maxMoves;
    }
+
+   public boolean canMakeSpecialMove() {
+      return !(shuffled && cleared && rotated && inverted)
+   }
 }
