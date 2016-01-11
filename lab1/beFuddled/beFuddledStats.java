2148093ca85fd9edc0cf86987ee7a534106805fa
apraturu <apraturu@calpoly.edu>
2016-01-11 11:38:04 -0800

    update

--- lab1/beFuddled/beFuddledStats.java
+++ lab1/beFuddled/beFuddledStats.java
@@ -11,6 +11,8 @@ public class beFuddledStats {
       int numWins = 0;
       ArrayList<Integer> pointsWon = new ArrayList<Integer>();
       ArrayList<Integer> pointsLost = new ArrayList<Integer>();
+      double winAvg = 0.0;
+      double lossAvg = 0.0;
 
       if (args.length != 1 && args.length != 2) {
          System.out.println("USAGE: java beFuddledGen <inputFileName> " + 
@@ -20,8 +22,8 @@ public class beFuddledStats {
       
       try {
          tok = new JSONTokener(new FileReader(args[0]));
-         String aType;
          JSONObject action;
+         String aType;
 
          while (tok.skipTo('{') != 0) {
             obj = new JSONObject(tok);
@@ -34,22 +36,39 @@ public class beFuddledStats {
                numCompletedGames++;
                if(action.getString("gameStatus").equals("Win")) {
                   numWins++;
-                  //pointsWon.add(new Integer(action.
+                  pointsWon.add(new Integer(action.getInt("points")));
+               }
+               else {
+                  pointsLost.add(new Integer(action.getInt("points")));
                }
             }
          }
          System.out.println("STATS");
-         System.out.println("Started games: " + numGames);
-         System.out.println("Completed games: " 
+         System.out.println("Games started: " + numGames);
+         System.out.println("Games completed: " 
           + numCompletedGames);
          System.out.println("Wins: " + numWins);
          System.out.println("Losses: " + (numCompletedGames - numWins));
          System.out.println("\nPOINTS");
+         //TODO add total # of points
+         System.out.printf("Average number of points in games won: %.2f\n",
+          calcAvg(pointsWon));
+         System.out.printf("Average number of points in games lost: %.2f\n",
+          calcAvg(pointsLost));
             
+         
       }
       catch(Exception e) {
          System.err.println("ERROR: could not parse file");
       }
       
+   }
+
+   public static double calcAvg(ArrayList<Integer> ints) {
+      double sum = 0.0;
+      for(Integer i : ints) {
+         sum += i;
+      }
+      return sum / (double)ints.size();
    }
 }
