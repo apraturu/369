dd924fd9e81743c62eb8fe478c42f6b861f1fd58
apraturu <apraturu@calpoly.edu>
2016-01-11 11:19:11 -0800

    fixed parsing

--- lab1/beFuddled/beFuddledStats.java
+++ lab1/beFuddled/beFuddledStats.java
@@ -1,14 +1,17 @@
 import org.json.*;
+import java.util.ArrayList;
 import java.io.*;
 
 public class beFuddledStats {
    public static void main(String[] args) {
       JSONObject obj;
-      String line;
+      JSONTokener tok;
       int numGames = 0;
       int numCompletedGames = 0;
       int numWins = 0;
-      
+      ArrayList<Integer> pointsWon = new ArrayList<Integer>();
+      ArrayList<Integer> pointsLost = new ArrayList<Integer>();
+
       if (args.length != 1 && args.length != 2) {
          System.out.println("USAGE: java beFuddledGen <inputFileName> " + 
           "[OPTIONAL]<numObjects>");
@@ -16,45 +19,36 @@ public class beFuddledStats {
       }
       
       try {
-         BufferedReader br = new BufferedReader(new FileReader(args[0]));
+         tok = new JSONTokener(new FileReader(args[0]));
+         String aType;
+         JSONObject action;
 
-         try {
-            line = br.readLine(); //read through open bracket
-            line = br.readLine();
-            String aType;
-            JSONObject action;
-
-            while (line != null && !line.startsWith("]")) {
-               obj = new JSONObject(line);
-               action = obj.getJSONObject("action");   
-               aType = action.getString("actionType");
-               if (aType.equals("GameStart")) {
-                  numGames++;
-               }
-               else if (aType.equals("GameEnd")) {
-                  numCompletedGames++;
-                  if(action.getString("gameStatus").equals("Win")) {
-                     numWins++;
-                  }
-               }
-
-               line = br.readLine();
+         while (tok.skipTo('{') != 0) {
+            obj = new JSONObject(tok);
+            action = obj.getJSONObject("action");   
+            aType = action.getString("actionType");
+            if (aType.equals("GameStart")) {
+               numGames++;
             }
-            System.out.println("STATS");
-            System.out.println("Started games: " + numGames);
-            System.out.println("Completed games: " 
-             + numCompletedGames);
-            System.out.println("Wins: " + numWins);
-            System.out.println("Losses: " + (numCompletedGames - numWins));
+            else if (aType.equals("GameEnd")) {
+               numCompletedGames++;
+               if(action.getString("gameStatus").equals("Win")) {
+                  numWins++;
+                  //pointsWon.add(new Integer(action.
+               }
+            }
          }
-         catch(JSONException j) {
-            System.err.println("ERROR: could not read JSON");
-            System.exit(-1);
-         }
-         br.close();
+         System.out.println("STATS");
+         System.out.println("Started games: " + numGames);
+         System.out.println("Completed games: " 
+          + numCompletedGames);
+         System.out.println("Wins: " + numWins);
+         System.out.println("Losses: " + (numCompletedGames - numWins));
+         System.out.println("\nPOINTS");
+            
       }
-      catch(IOException e) {
-         System.err.println("ERROR: could not open or read from input file");
+      catch(Exception e) {
+         System.err.println("ERROR: could not parse file");
       }
       
    }
