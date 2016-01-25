c9d42f93b0882ea08782a8c3382af5467420a4bd
User <apraturu@cslvm31.csc.calpoly.edu>
2016-01-25 11:40:19 -0800

    adding java files

---
+++ lab4/ClientSpoof.java
@@ -0,0 +1,21 @@
+/**
+ * Varsha Roopreddy and Anusha Praturu
+ * CPE 369 - Secion 03
+ * Lab 4
+ * ClientSpoof.java
+ */
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.Block;
+import com.mongodb.client.FindIterable;
+import org.bson.Document;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
+public class ClientSpoof {
+   public static void main(String[] args) {
+      System.out.println("testing");
+   }
+}
